060c4578248ec969fd3867b026f194df28d43384
tnkmatic <eiichi1208@gmail.com>
2012-08-17 00:20:17 +0900

    新規追加

---
+++ src/com/tnkmatic/trial/util/StaticValues.java
@@ -0,0 +1,8 @@
+package com.tnkmatic.trial.util;
+
+public class StaticValues {
+	public static int EDIT_MODE_ENTRY		= 1;
+	public static int EDIT_MODE_MODIFY	= 2;
+	public static int EDIT_MODE_DELETE	= 3;
+
+}
